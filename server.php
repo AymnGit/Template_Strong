21a2ec57cc7ffe7f67771a7a0d6a9afa2af03026
AymnGit <44608985+AymnGit@users.noreply.github.com>
2018-11-23 16:03:41 +0100

    Add files via upload

---
+++ server.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="css/style.css">
+  <meta charset="UTF-8">
+  <title>Web Template For HTML</title>
+</head>
+<body>
+  <div class="content-wrapper">
+    
+      <nav>
+
+        <ul>
+
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+        </ul>
+
+      </nav>
+
+  </div>
+</body>
+</html>
